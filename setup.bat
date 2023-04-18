b4863b707c213465449b24a8e5fe2aa04e9a2ce4
Woz4tetra <woz4tetra@gmail.com>
2023-04-18 03:03:12 -0400

    add windows batch files. fix null char

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+
+SET BASE_DIR=%~dp0
+
+python "-m" "pip" "install" "virtualenv"
+python "-m" "venv" "%BASE_DIR%\venv"
+call "%BASE_DIR%\venv\bin\activate.bat"
+python "-m" "pip" "install" "--upgrade" "pip" "setuptools"
+python "-m" "pip" "install" "-r" "%BASE_DIR%\requirements.txt" "--user"
