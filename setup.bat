c9ce964bd7e98565f13a255bc34391ca2f62c23c
Woz4tetra <woz4tetra@gmail.com>
2023-04-18 03:42:57 -0400

    add more batch scripts

--- setup.bat
+++ setup.bat
@@ -4,6 +4,6 @@ SET BASE_DIR=%~dp0
 
 python "-m" "pip" "install" "virtualenv"
 python "-m" "venv" "%BASE_DIR%\venv"
-call "%BASE_DIR%\venv\bin\activate.bat"
+call "%BASE_DIR%\venv\Scripts\activate.bat"
 python "-m" "pip" "install" "--upgrade" "pip" "setuptools"
-python "-m" "pip" "install" "-r" "%BASE_DIR%\requirements.txt" "--user"
+python "-m" "pip" "install" "-r" "%BASE_DIR%\requirements.txt"
