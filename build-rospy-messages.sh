66c74946b309d7cb33b213b11a9606033413275d
Woz4tetra <woz4tetra@gmail.com>
2023-05-24 10:59:57 -0400

    make custom messages optional

--- build-rospy-messages.sh
+++ build-rospy-messages.sh
@@ -8,24 +8,21 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 BASE_DIR=$(realpath "$(dirname $0)")
-INTERFACES_BASE_DIR=$1
+CUSTOM_ROS_MSG_DIR=$1
 SOURCES_PATH=${2:-${BASE_DIR}/source_list.json}
-
-if [ -z ${INTERFACES_BASE_DIR} ]; then
-    echo "Interfaces source dir not set!"
-    exit 1
-else
-    SRC_ROS_MSG_DIR=${INTERFACES_BASE_DIR}
-fi
-
-CUSTOM_PKG_NAME=$(basename ${INTERFACES_BASE_DIR})
 
 GEN_MSG_ROOT=${BASE_DIR}/genmsg
 
-mkdir -p ${GEN_MSG_ROOT}
-ROS_MSG_DIR=${GEN_MSG_ROOT}/${CUSTOM_PKG_NAME}
-rm -r ${ROS_MSG_DIR} || true
-cp -r ${SRC_ROS_MSG_DIR} ${ROS_MSG_DIR}
+if [ -z ${CUSTOM_ROS_MSG_DIR} ]; then
+    echo "Custom messages not specified. Not generating custom messages"
+else
+    mkdir -p ${GEN_MSG_ROOT}
+    ROS_MSG_DIR=${GEN_MSG_ROOT}/${CUSTOM_PKG_NAME}
+    rm -r ${ROS_MSG_DIR} || true
+    cp -r ${SRC_ROS_MSG_DIR} ${ROS_MSG_DIR}
+fi
+
+CUSTOM_PKG_NAME=$(basename ${INTERFACES_BASE_DIR})
 
 source ${BASE_DIR}/venv/bin/activate
 python3 ${BASE_DIR}/clone_repos.py ${SOURCES_PATH} ${GEN_MSG_ROOT}
